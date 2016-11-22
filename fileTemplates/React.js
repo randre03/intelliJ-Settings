a3ee7765b16e31c5bdfa15e8f16e70d624874742
Roger E Randall <roger@rogerrandall.com>
2016-11-21 22:07:24 -0500

    IU-2016.3 <randre03@Rogers-MacBook-Pro.local Create markdown.xml, vim_settings.xml, IntelliLang.xml, ide.general.xml, project.default.xml, diff.xml, React.js, ActionScript.xml, git.xml, other.xml, Clojure.xml, terminal.xml, file.template.settings.xml, ignore.xml, ui.lnf.xml, remote-servers.xml, code.style.schemes.xml, remote-server-client-libraries.xml, colors.scheme.xml, notifications.xml, path.macros.xml, Darcula copy.icls, databaseDrivers.xml, github_settings.xml, haskell.xml, Groovy.xml, ReactJS.xml, emmet.xml, Default.xml, scala.xml, jdk.table.xml, ColdFusion.xml, Kotlin.xml, laf.xml, Default _1_.xml, web-browsers.xml, editor.codeinsight.xml, databaseSettings.xml, keymap.xml, editor.xml, applicationLibraries.xml, haskelldebug.xml, debugger.xml

---
+++ fileTemplates/React.js
@@ -0,0 +1,13 @@
+#parse("File Header")
+ 
+ import React from 'react';
+ 
+ const ${NAME} = () => {
+    return (
+        <div className="">
+        
+        </div>
+    );
+ };
+ 
+ export default ${NAME}
